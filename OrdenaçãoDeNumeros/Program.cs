cdf7728cd4c855987fd923895ca378742f296975
Gelvane Nascimento Silva <gsilva.sts@marimex.com.br>
2021-05-13 15:19:50 -0300

    commit inicial

---
+++ OrdenaçãoDeNumeros/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace OrdenaçãoDeNumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor = { 5, 3, 2, 4, 7, 1, 0, 6 };
+
+            int j, i, n;
+
+            for (i = 0; i < vetor.Length; i++)
+            {
+                n = vetor[i];
+
+                j = i;
+
+                while ((j > 0) && (vetor[j-1]>n))
+                {
+                    vetor[j] = vetor[j - 1];
+                    j = j - 1;
+                }
+                vetor[j] = n;
+            }
+            Console.Write("Vetor = { ");
+            foreach (var item in vetor)
+            {               
+                Console.Write(item + " ");
+           
+            }
+      
+            Console.WriteLine("}");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# OrdenacaoDeNumeros
+
+
+Aplicativo em console application para realizar ordenação por inserção
